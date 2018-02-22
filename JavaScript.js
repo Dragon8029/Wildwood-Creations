c8b2f6f65a4a7a7a5a5c779becd3c8c8f4bcfe58
Cam Utz <Dragon8029@gmail.com>
2018-02-21 20:16:37 -0700

    Some new additions to the project.

--- JavaScript.js
+++ JavaScript.js
@@ -1,35 +1,40 @@
-
-
-var sections = $('section')
-  , nav = $('nav')
-  , nav_height = nav.outerHeight();
- 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
- 
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
- 
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
- 
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    // //smoothscroll
+    // $('a[href^="#"]').on('click', function (e) {
+    //     e.preventDefault();
+    //     $(document).off("scroll");
+        
+    //     $('li').each(function () {
+    //         $(this).removeClass('active');
+    //     })
+    //     $(this).addClass('active');
+      
+    //     var target = this.hash,
+    //         menu = target;
+    //     $target = $(target);
+    //     $('html, body').stop().animate({
+    //         'scrollTop': $target.offset().top+2
+    //     }, 500, 'swing', function () {
+    //         window.location.hash = target;
+    //         $(document).on("scroll", onScroll);
+    //     });
+    // });
 });
 
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#bs-nav-demo li').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#bs-nav-demo ul li').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
 
-
-nav.find('a').on('click', function () {
-  var $el = $(this)
-    , id = $el.attr('href');
- 
-  $('html, body').animate({
-    scrollTop: $(id).offset().top - nav_height
-  }, 500);
- 
-  return false;
-});
